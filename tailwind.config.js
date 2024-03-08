e70df5096b7b3d88ff36cfbb67642b6e4e45c9c7
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2024-03-08 15:14:07 +0100

    Built all viewports

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      colors: {
+        DarkCyan: "hsl(158, 36%, 37%)",
+        Cream: "hsl(30, 38%, 92%)",
+        VeryDarkBlue: "hsl(212, 21%, 14%)",
+        DarkGrayishBlue: "hsl(228,12%,48%)",
+        White: "hsl(0,0%,100%)",
+      },
+      fontFamily: {
+        Montserrat: "Montserrat, sans-serif",
+        Fraunces: "Fraunces, serif",
+      },
+    },
+  },
+  plugins: [],
+};
